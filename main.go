d6512c93292afd2138fb3590756ad81a2df69154
Rahul Mishra <rahulmishra102000@gmail.com>
2021-10-26 23:17:38 +0530

    Gallery app completed

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"strings"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	// "fyne.io/fyne/v2/container"
-	// "fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/container"
 	"fmt"
-    "io/ioutil"
-    "log"
+	"io/ioutil"
+	"log"
+	"fyne.io/fyne/v2/canvas"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Gallery")
+	w.Resize(fyne.NewSize(990, 660))
 
 	root_src := "C:\\Users\\91982\\Pictures"
 
@@ -20,10 +24,26 @@ func main() {
 	if err != nil {
         log.Fatal(err)
     }
+
+	tabs := container.NewAppTabs()
 	
 	for _, file := range files {
         fmt.Println(file.Name(), file.IsDir())
+		if !file.IsDir(){
+			extension := strings.Split(file.Name(), ".")[1]
+			if extension == "png" || extension == "jpeg"{
+				image := canvas.NewImageFromFile(root_src+"\\"+file.Name())
+				image.FillMode = canvas.ImageFillOriginal
+				tabs.Append(container.NewTabItem(file.Name(), image))
+			}
+		}
     }
+
+
+
+	tabs.SetTabLocation(container.TabLocationLeading)
+
+	w.SetContent(tabs)
 
 	w.ShowAndRun()
 }
